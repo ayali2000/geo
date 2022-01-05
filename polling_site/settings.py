e75e9512e8b0fdef2e9806dbdd70efa1414e455e
ayali2000 <aliarafatyassin@gmail.com>
2022-01-05 04:36:07 -0800

    chnges

--- polling_site/settings.py
+++ polling_site/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kmq5$9f_fn^0tz)2tcmqjb37bqrd8f8#qt@#y!5630vs25o%8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
